991d16ae610824d6b4bb32b07004d870e95b3bec
Admin <admin@Admins-MacBook-Air.local>
2023-02-16 23:38:02 +0530

    game completed

--- DoremonGame/app.js
+++ DoremonGame/app.js
@@ -1,5 +1,13 @@
 
 let score = 0
+let cross = true;
+
+let audio = new Audio("sfx-defeat1.mp3")
+let audiogo = new Audio("doremonSong.mp3")
+
+setTimeout(() => {
+    audiogo.play()
+}, 1000)
 
 document.onkeydown = function (e) {
     console.log("key code is", e.keyCode);
@@ -39,13 +47,35 @@ setInterval(() => {
 
     offsetX = Math.abs(nx - ox)
     offsetY = Math.abs(ny - oy)
-    console.log(offsetX, offsetY);
-    if (offsetX < 93 && offsetY < 52) {
-        gameOver.style.visibility = "visible";
+    // console.log(offsetX, offsetY);
+    if (offsetX < 73 && offsetY < 52) {
+        gameOver.innerHTML="Game over";
         obstacle.classList.remove("obstacleAni")
+
+        audio.play();
+        setTimeout(()=>{
+           audio.pause() 
+        },5000)
     }
-    else {
+    else if (offsetX < 145 && cross) {  //random num=>145
         score += 1
+        updateScore(score)
+        cross = false;
+
+        setTimeout(() => {
+            cross = true
+        }, 1000)
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's'
+        }, 500)
     }
 
-}, 100)
+}, 10)
+
+
+function updateScore(score) {
+    scoreCont.innerHTML = "your score: " + score;
+
+}
